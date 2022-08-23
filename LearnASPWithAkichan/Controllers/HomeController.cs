98c31217ff001585311c34d36dde83bdadea289e
akkii0609 <38753836+akisantos@users.noreply.github.com>
2022-08-23 14:59:26 +0700

    Còn lỗi ở tiên quyết và ngoài kế hoạch

--- LearnASPWithAkichan/Controllers/HomeController.cs
+++ LearnASPWithAkichan/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace LearnASPWithAkichan.Controllers
         {
             _logger = logger;
             _db = db;
-           
+
         }
 
 
@@ -64,7 +64,7 @@ namespace LearnASPWithAkichan.Controllers
             }
 
             TempData["notification"] = "Chưa đến hạn đăng ký";
-       
+
             return View();
 
 
@@ -91,13 +91,15 @@ namespace LearnASPWithAkichan.Controllers
             var sub = _db.Subjects.FirstOrDefault(x => x.Id == clas.SubjectId);
             var stu = _db.Students.FirstOrDefault(x => x.AccountId == acc.Id);
             //
+
+
             if (!KiemTraLopDaDangKy(id, stu.Id.ToString()))
             {
                 RegistClass rgclass = new RegistClass()
                 {
                     StudentId = stu.Id,
                     ClassSessionId = clas.Id,
-                    Status = true,
+                    Status = false,
                     RegistDate = DateTime.Now,
                     Credits = sub.Credits
                 };
@@ -116,7 +118,7 @@ namespace LearnASPWithAkichan.Controllers
         }
         // ---> check học phân đã đăng ký ()
         public bool checkClass(string id)
-           {
+        {
             string username = HttpContext.Session.GetString("username");
             var acc = _db.Accounts.FirstOrDefault(x => x.UserName == username);
             var clas = _db.ClassSessions.FirstOrDefault(x => x.Id == id);
@@ -131,52 +133,92 @@ namespace LearnASPWithAkichan.Controllers
             }
             else
             {
-                return true; 
+                return true;
             }
         }
-            // ham lay danh sach da dang ky cua sinh vien
-         private List<RegistClass> DanhSachLopDaDangKy(string student_id)
-          {
-             // list sinh vien da dang ky
-                List<RegistClass> lst_regist_class = new List<RegistClass>();
-                lst_regist_class = _db.RegistClasses.Where(s => s.StudentId == student_id && s.Status == false).ToList();
-                return lst_regist_class;      
-          }     
-            // hàm kiểm mon hoc da hoc chua
-         public bool KiemTraLopDaDangKy(string subject_id, string student_id)
-            {
-                List<RegistClass> danhSachDaDangKy = DanhSachLopDaDangKy(student_id);
-                List<ClassSession> danhSachLopHP = _db.ClassSessions.Where(s => s.SubjectId == subject_id).ToList();
-                foreach (var item in danhSachDaDangKy)
-                {
-                    foreach (var item2 in danhSachLopHP)
-                    {
-                        if (item.ClassSessionId == item2.Id)
-                        {
-                            return true;
-                        }
-                    }
-                }
-                return false;
-            }     
-            // kiem tra mon tien quyet
-         public List<Subject> kiemTraMonTienQuyet(string subject_id)
-            {
-                List<Subject> subjects = new List<Subject>();
+        // ham lay danh sach da dang ky cua sinh vien
+        private List<RegistClass> DanhSachLopDaDangKy(string student_id)
+        {
+            // list sinh vien da dang ky
+            List<RegistClass> lst_regist_class = new List<RegistClass>();
+            lst_regist_class = _db.RegistClasses.Where(s => s.StudentId == student_id && s.Status == false).ToList();
+            return lst_regist_class;
+        }
+        // hàm kiểm mon hoc da hoc chua
+        public bool KiemTraLopDaDangKy(string subject_id, string student_id)
+        {
+            List<RegistClass> danhSachDaDangKy = DanhSachLopDaDangKy(student_id);
+            List<ClassSession> danhSachLopHP = _db.ClassSessions.Where(s => s.SubjectId == subject_id).ToList();
+            List<PrerequisiteSubject> monTienQuyet = DanhSachMonTienQuyet(student_id);
+            List<ClassSession> LopDaHoc = new List<ClassSession>();
 
-                var lst_monTienQuyet = _db.PrerequisiteSubjects.Where(s => s.SubjectId == subject_id).ToList();
-                foreach (var item in lst_monTienQuyet)
+            bool checker = false;
+
+            //Xac dinh mon co bi trung hay khong (da dang ky chua)
+            foreach (var item in danhSachDaDangKy)
+            {
+                foreach (var item2 in danhSachLopHP)
                 {
-                    if (!String.IsNullOrEmpty(item.PrerequisiteSubjectId))
+                    if (item.ClassSessionId == item2.Id)
                     {
-                        foreach (var item2 in _db.Subjects.Where(s => s.Id == item.PrerequisiteSubjectId))
+                        checker = true;
+                    } 
+
+                }
+            }
+
+            // Check tien quyet
+            if (!checker)
+            {
+                foreach (var item in danhSachLopHP)
+                {
+                    foreach (var item2 in monTienQuyet)
+                    {
+                        if (item.Id == item2.SubjectId && item2.PrerequisiteSubjectId == null)
                         {
-                            subjects.Add(item2);
+                            checker= false;
                         }
+                        else
+                        {
+                            foreach (var item3 in monTienQuyet)
+                            {
+                                if (item3.PrerequisiteSubjectId == item.SubjectId)
+                                {
+                                    checker = true;
+                                }
+                                else
+                                {
+                                    checker= false;
+                                }
+                            }
+                        }
+                        
+
                     }
                 }
-                return subjects;
-          }
+            }
+
+            return checker;
+        }
+        // kiem tra mon tien quyet
+        public List<PrerequisiteSubject> DanhSachMonTienQuyet(string subject_id)
+        {
+            List<PrerequisiteSubject> subjects = new List<PrerequisiteSubject>();
+
+            var lst_monTienQuyet = _db.PrerequisiteSubjects.Where(s => s.SubjectId == subject_id).ToList();
+            foreach (var item in lst_monTienQuyet)
+            {
+                if (!String.IsNullOrEmpty(item.PrerequisiteSubjectId))
+                {
+                    foreach (var item2 in _db.PrerequisiteSubjects.Where(s => s.SubjectId == subject_id))
+                    {
+                       
+                        subjects.Add(item2);
+                    }
+                }
+            }
+            return subjects;
+        }
 
         // ---> hủy đăng ký(bug)
         public IActionResult deleteLHP(string idClass)
@@ -253,20 +295,43 @@ namespace LearnASPWithAkichan.Controllers
         // ---> lớp học phần trong kế hoạch | ngoài kế hoạch
         public IActionResult ThongBao()
         {
-            
+            if (HttpContext.Session.GetString("role") == "True")
+            {
+                RoleCheckByHand = true;
+            }
+            else
+            {
+                RoleCheckByHand = false;
+            }
+
+            if (HttpContext.Session.GetString("trongKyDangKy") == "True")
+            {
+                trongKyDangKy = true;
+            }
+            else
+            {
+                trongKyDangKy = false;
+            }
+
             if (trongKyDangKy && RoleCheckByHand)
             {
                 var list_class_nkh = from s in _db.ClassSessions where s.CommonClass == true select s;
+                
+                TempData["notification"] = "Môn ngoài kế hoạch";
                 return View(list_class_nkh.ToList());
             }
-            TempData["notification"] = "Chưa đến kỳ đăng ký!";
-            return View();
-        }            
+            else
+            {
+                TempData["notification"] = "Chưa đến kỳ đăng ký!";
+                return View();
+            }
+
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }   
-    } 
+        }
+    }
 }
